53a06701cdf7fcfedcf843e01be3b28c2fb1cb94
Diks7106 <dikshantsavaliya2000@gmail.com>
2022-12-15 10:16:27 +0530

    attendence shteet create

--- src/pages/Attendance/AttendenceIndex.jsx
+++ src/pages/Attendance/AttendenceIndex.jsx
@@ -5,22 +5,52 @@ import {
   Select,
   Typography,
   TextField,
+  Checkbox,
 } from "@mui/material";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import DialogForAttendance from "./DilogForAttendance";
+import { css } from "@emotion/react";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { Form, Formik } from "formik";
 const AttendenceCollection = () => {
   const [open, setOpen] = useState(false);
   const [currentRow, setCurrentRow] = useState();
   const [value, setValue] = useState(null);
+  const formikRef = useRef();
 
   const initialValues = {
     bname: "",
     tsem: "",
     tsubname: "",
   };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 20 },
+    {
+      field: "lastName",
+      headerName: "Student Name",
+      width: 250,
+      editable: true,
+    },
+    {
+      field: "1",
+      headerName: "1",
+      width: 60,
+      renderCell: ({ row }) => (
+        <strong>
+          <GridActionsCellItem icon={<Checkbox />} label="1" />
+        </strong>
+      ),
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon" },
+    { id: 2, lastName: "Joy", firstName: "Don" },
+  ];
 
   const handleClickOpen = () => {
     setCurrentRow(initialValues);
@@ -123,6 +153,39 @@ const AttendenceCollection = () => {
       >
         Take a Attendance
       </Button>
+      <Grid item xs={12}>
+        <Formik
+          innerRef={formikRef}
+          initialValues={currentRow}
+          onSubmit={(values) => values}
+        >
+          {(formik) => (
+            <Form>
+              <Grid item xs={12} sx={{ borderTop: "1px solid black" }}>
+                <Grid>
+                  <DialogTitle style={{ paddingBottom: "0px" }}>
+                    Attendance Sheet
+                  </DialogTitle>
+                  <div style={{ height: 500, width: "100%" }}>
+                    <DataGrid
+                      editMode="row"
+                      const
+                      rows={rows}
+                      columns={columns}
+                      css={css`
+                        height: calc(100vh - 1500px - 30px) !important;
+                      `}
+                      experimentalFeatures={{ newEditingApi: true }}
+                      hideFooter
+                    />
+                  </div>
+                </Grid>
+                <br />
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </Grid>
     </>
   );
 };
