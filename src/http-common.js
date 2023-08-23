c62d0b9f65fc4fb8581735d25e14969fb845491c
gunatit369 <adityakalmekh2112@gmail.com>
2023-08-23 16:01:50 +0530

    http-common change

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://college-backend.netlify.app/",
+  // baseURL: "https://college-backend.netlify.app/",
+  baseURL: "http://localhost:5000/Attendance",
   headers: {
     "Content-type": "application/json",
   },
